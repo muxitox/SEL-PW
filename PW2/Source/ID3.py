04b6eec95f3e18bdf9056b6320cad332c37f9127
muxitox <alpcl6@hotmail.com>
2019-04-25 17:58:30 +0200

    Info gain

--- PW2/Source/ID3.py
+++ PW2/Source/ID3.py
@@ -6,7 +6,7 @@ import math
 Performs PRISM over the provided dataset
 '''
 
-class Tree:
+class ID3:
     # Loads data from a path
     def __init__(self, data, labels):
         self.data = data
@@ -30,12 +30,29 @@ class Tree:
     def calculate_ig(X,Y,Ak):
         classes = Y.unique()
 
-        # Calculate the Mutual Info between the dataset and the class
+        # Calculate the Entropy of X with respect to C
         InfoXC = 0
         for c_i in classes:
             pXc_i = Y[Y == c_i].size / X.size
             InfoXC -= (pXc_i * math.log2(pXc_i))
 
+        # Calculate the conditional entropy given Ak
+        InfoXA = 0
+        for vi in X[Ak].unique():
+            Xvi = X.loc[X[Ak] == vi]
+
+            pXvi = Xvi.size / X.size
+
+            Yvi = Y.loc[Xvi.index]
+
+            InfoXCVi = 0
+            for c_i in classes:
+                pXvici = Yvi[Yvi == c_i].size / Xvi.size
+                InfoXCVi -= (pXvici * math.log2(pXvici))
+
+            InfoXA += pXvi * InfoXCVi
+
+        return InfoXC-InfoXA
 
     @staticmethod
     # Generates the decision tree following the ID3 algorithm for the dataset X, labesl Y and the set of attributes A
@@ -45,6 +62,7 @@ class Tree:
             ig = self.calculate_ig(X, Ak)
             if ig > max_ig:
                 best_attribute = Ak
+
 
         # TODO: take vi as root and recursevily build the tree
 
